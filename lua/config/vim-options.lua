088c8375e64d6c358a1f4c36a90d309129ac57c8
Dane Russell <danerussell4@gmail.com>
2024-08-14 11:00:25 -0400

    Make quitting multiple buffers easier

--- lua/config/vim-options.lua
+++ lua/config/vim-options.lua
@@ -1,7 +1,7 @@
 vim.keymap.set('i', 'jj', '<Esc>')
 vim.keymap.set('n', '<leader>w', '<cmd>w<cr>')
-vim.keymap.set('n', '<leader>q', '<cmd>q<cr>')
-vim.keymap.set('n', '<leader>wq', '<cmd>wq<cr>')
+vim.keymap.set('n', '<leader>wq', '<cmd>wqa<cr>')
+vim.keymap.set('n', '<leader>q', '<cmd>qa<cr>')
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
