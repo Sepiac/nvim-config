2e0a55738b328817030ca4c09090f5abdddcd715
Dane Russell <danerussell4@gmail.com>
2024-08-15 11:25:24 -0400

    remove wq from leader commands

--- lua/config/vim-options.lua
+++ lua/config/vim-options.lua
@@ -1,6 +1,5 @@
 vim.keymap.set('i', 'jj', '<Esc>')
 vim.keymap.set('n', '<leader>w', '<cmd>w<cr>')
-vim.keymap.set('n', '<leader>wq', '<cmd>wqa<cr>')
 vim.keymap.set('n', '<leader>q', '<cmd>qa<cr>')
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
