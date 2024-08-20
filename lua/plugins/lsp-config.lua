1ec87c3796a27ec597ef270f33f82af29829c19b
Dane Russell <danerussell4@gmail.com>
2024-08-19 22:33:52 -0400

    Enable formatting only a selected section

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -23,7 +23,7 @@ return {
       lspconfig.gopls.setup {}
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, {})
-      vim.keymap.set('n', '<leader>kf', vim.lsp.buf.format, {})
+      vim.keymap.set({'n', 'v'}, '<leader>kf', vim.lsp.buf.format, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
