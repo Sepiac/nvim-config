93dc6d20402727eeb7222baafb1db69589c9431b
Dane Russell <danerussell4@gmail.com>
2024-08-19 21:25:58 -0400

    format file

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,21 +1,21 @@
 return {
   {
     "williamboman/mason.nvim",
-    config = function ()
+    config = function()
       require("mason").setup()
     end
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = function ()
+    config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "jsonls", "tsserver", "gopls"}
+        ensure_installed = { "lua_ls", "jsonls", "tsserver", "gopls" }
       })
     end
   },
   {
     "neovim/nvim-lspconfig",
-    config = function ()
+    config = function()
       local lspconfig = require('lspconfig')
       lspconfig.lua_ls.setup({})
       lspconfig.jsonls.setup({})
@@ -25,7 +25,7 @@ return {
       vim.keymap.set('n', 'r', vim.lsp.buf.rename, {})
       vim.keymap.set('n', 'kf', vim.lsp.buf.format, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   },
   {
