dab46c35dfc577b3eb6a67f936f85a64881a8dda
Dane Russell <danerussell4@gmail.com>
2024-08-20 00:38:33 -0400

    Fix formatting

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,12 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "jsonls", "tsserver", "gopls" }
+        ensure_installed = {
+          "lua_ls",
+          "jsonls",
+          "tsserver",
+          "gopls"
+        }
       })
     end
   },
@@ -23,7 +28,7 @@ return {
       lspconfig.gopls.setup {}
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, {})
-      vim.keymap.set({'n', 'v'}, '<leader>kf', vim.lsp.buf.format, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>kf', vim.lsp.buf.format, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
