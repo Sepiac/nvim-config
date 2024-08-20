3ac8718bd892e22771cbb243ad471e8c3881fddf
Dane Russell <danerussell4@gmail.com>
2024-08-19 21:25:34 -0400

    Add lsp format keybinding

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -23,6 +23,7 @@ return {
       lspconfig.gopls.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'r', vim.lsp.buf.rename, {})
+      vim.keymap.set('n', 'kf', vim.lsp.buf.format, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
     end
